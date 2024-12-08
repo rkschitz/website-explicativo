67b456856f57c84637016430a794ee6ac1ee7586
Ana Karolina <akarolhc@gmail.com>
2024-12-08 16:49:59 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# website-explicativo
-Repositório criado para o AEO do professor Renan referente ao website explicativo
--- backend/src/model/association.js
+++
@@ -1,5 +0,0 @@
-const User = require("./user");
-const Suggestion = require("./suggestion");
-
-User.hasMany(Suggestion, {foreignKey: 'userId', as: 'suggestions'});
-Suggestion.belongsTo(User, {foreignKey: 'userId', as: 'user'});
